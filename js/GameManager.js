450a20dfb9d132bd2868749a299583d4b27fb064
josepaulo <josepaulo@ceuma.br>
2021-02-15 23:33:20 -0300

    basic trigger assigment

--- js/GameManager.js
+++ js/GameManager.js
@@ -2,15 +2,31 @@
 class GameManager {
     constructor(override_config = null){
         this.config = {...__config, ...override_config}
+        this.triggers = []
+        this.grid = []
         console.log(this.config)
     }
-
     generateGrid(){
         const size = this.config.size.split("x")
-        var grid = new Array(size[0])
+        this.grid = new Array(size[0])
         for(let i = 0; i < size[0]; i++){
-            grid[i] = "0".repeat(size[1]).split("")
+            this.grid[i] = "0".repeat(size[1]).split("")
         }
-        return grid;
+        return this.grid;
+    }
+    assignTrigger(event_name, callback){
+        this.triggers[event_name] = callback
+    }
+    clearGrid(){
+        for (let i = 0; i < this.grid.length; i++) {
+            for (let j = 0; j < this.grid[i].length; j++) {
+                this.grid[i][j] = 0;
+            }
+        }
+    }
+    triggerEvent(event_name, xy){
+        if(this.grid[xy.x][xy.y] == 0 && event_name=="onClick") {
+            return this.triggers["click-empty-cell"]()
+        }
     }
 }
---
+++ js/uTests/events-test.js
@@ -0,0 +1,14 @@
+
+touchReturn = () => {
+    return "toque!"
+}
+gameMg = new GameManager()
+gameMg.assignTrigger("click-empty-cell", touchReturn)
+grid = gameMg.generateGrid()
+gameMg.clearGrid()
+
+expect(
+    gameMg.triggerEvent("onClick", {x:0, y:0})
+).toBe(
+    "toque!"
+)
